53fc31b7622db3e4e538a8eb2f8f2a02fc1f6b7e
Egidius Mysliwietz <egidius@mysliwietz.de>
2021-04-20 22:25:12 +0200

    Dired with icons

--- modules/dired-tweaks.el
+++ modules/dired-tweaks.el
@@ -7,9 +7,18 @@
 
 (use-package all-the-icons-dired
   :ensure t
-  :defer t
+  :defer nil
   :config
-  (all-the-icons-dired-mode 1))
+  ;(all-the-icons-dired-mode 1)
+  (add-hook 'dired-mode-hook 'all-the-icons-dired-mode))
+
+(defun dired-open-file ()
+  "In dired, open the file named on this line."
+  (interactive)
+  (let* ((file (dired-get-filename nil t)))
+    (message "Opening %s..." file)
+    (call-process "xdg-open" nil 0 nil file)))
+
 
 (define-minor-mode dired-follow-mode
   "Diplay file at point in dired after a move."
@@ -20,7 +29,18 @@
     (advice-remove 'dired-next-line (lambda (arg) (dired-display-file)))))
 
 (setq vc-follow-symlinks t)
+(setq dired-listing-switches "-lt")
 
+(use-package async
+  :ensure t
+  :defer t
+  :init (dired-async-mode 1))
+
+(use-package dired-quick-sort
+  :defer t
+  :config
+  (dired-quick-sort-setup)
+  (setq dired-quick-sort-suppress-setup-warning t))
 
 (provide 'dired-tweaks)
 
